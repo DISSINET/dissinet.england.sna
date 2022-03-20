4d8e2d21a750d779216cf811a83880d97ffa6cad
José Luis Estévez <50023683+joseluisesna@users.noreply.github.com>
2022-03-20 18:49:32 +0100

    Add files via upload

--- 2_KENT_BERGM.R
+++ 2_KENT_BERGM.R
@@ -28,35 +28,35 @@ summary(ntw_objs$inculpations ~ edges + mutual +
 
 # MODELLING
 set.seed(0708)
-model1 <- bergmM(ntw_objs$inculpations ~ edges + mutual + # bergmM to handle missing data
+model <- bergmM(ntw_objs$inculpations ~ edges + mutual + # bergmM to handle missing data
                   dgwdsp(decay=log(2),fixed=TRUE,type='OTP') +
                   dgwesp(decay=log(2),fixed=TRUE,type='OTP') +
                   edgecov(kinship) + edgecov(colocation) +
                   nodeofactor('sex') + nodeifactor('sex') + nodematch('sex') +
                    nodeifactor('witness') + nodeifactor('sentenced'),
-                burn.in=1000,main.iters=5000,gamma=0.40,
+                burn.in=1000,main.iters=5000,gamma=0.30,
                 prior.mean=c(rep(0,11)),prior.sigma=diag(5,11))
-summary(model1) 
+summary(model) 
 
 ########################################################################################################################
 
 # RESULTS 
 
 # Extraction of posteriors
-posteriors <- data.frame(model1$Theta)
+posteriors <- data.frame(model$Theta)
 # Estimates
-model1results <- data.frame(coeff=apply(posteriors,2,mean))
+modelresults <- data.frame(coeff=apply(posteriors,2,mean))
 
 # Name of effects estimated
-rownames(model1results) <- c('edges','mutual','gwdsp','gwesp','kinship','same setting',
+rownames(modelresults) <- c('edges','mutual','gwdsp','gwesp','kinship','same setting',
                              'female (out)','female (in)','same sex','other deponent (in)','under trial (in)')
 
 # Extraction of Bayesian p values
-for(i in 1:nrow(model1results)){
-  if(model1results[i,'coeff'] > 0){
-    model1results[i,'p'] <- sum(posteriors[,i] < 0)/length(posteriors[,i])
+for(i in 1:nrow(modelresults)){
+  if(modelresults[i,'coeff'] > 0){
+    modelresults[i,'p'] <- sum(posteriors[,i] < 0)/length(posteriors[,i])
   }else{
-    model1results[i,'p'] <- sum(posteriors[,i] > 0)/length(posteriors[,i])
+    modelresults[i,'p'] <- sum(posteriors[,i] > 0)/length(posteriors[,i])
   }
 }
 
@@ -68,16 +68,16 @@ sig <- function(x){
                        ifelse(x < .1,'+',''))))
 }
 
-model1results$sig <- sig(model1results$p)
-model1results
-write.table(model1results,'results BERGM.csv',row.names=TRUE,sep=',')
+modelresults$sig <- sig(modelresults$p)
+modelresults
+write.table(modelresults,'results BERGM.csv',row.names=TRUE,sep=',')
 
 ########################################################################################################################
 
 # GOODNESS OF FIT
 
 jpeg(filename='BERGM_gof.jpeg',width=10,height=8,units='in',res=1000)
-bgof(model1)
+bgof(model)
 dev.off()
 
 ########################################################################################################################
